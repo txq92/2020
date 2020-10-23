51e84fce4b10204fbe4cd349e5b2de3ad80d381e
root <root@core-ssh.local.hass.io>
2020-10-24 00:06:01 +0700

    hihh

---
+++ ha_gitpush.sh
@@ -0,0 +1,12 @@
+# Go to /config folder or 
+# Change this to your Home Assistant config folder if it is different
+cd /config/custom_components/2020
+
+# Add all files to the repository with respect to .gitignore rules
+git add .
+
+# Commit changes with message with current date stamp
+git commit -m "hihh"
+
+# Push changes towards GitHub
+git push -u origin master
